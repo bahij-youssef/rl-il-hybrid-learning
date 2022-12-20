295346d93bf7ea22bc5dd45a05f1b4325c27b114
Bahij Youssef <bahij.youssef@gmail.com>
2022-12-20 00:52:06 +0000

    Shell script adjustment.

--- run_play_experiments.sh
+++ run_play_experiments.sh
@@ -3,4 +3,4 @@ height=60
 model_dir=/home/model_testing
 games=50
 
-python3 play_vizdoom.py $model_dir --games $games --width $width --height $height
+python3 play_experiments.py $model_dir --games $games --width $width --height $height
